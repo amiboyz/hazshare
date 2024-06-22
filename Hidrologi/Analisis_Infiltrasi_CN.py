1a33295f682e4b852fb79fc38eb79e353cbe5643
amiboyz <152479402+amiboyz@users.noreply.github.com>
2024-06-22 09:33:15 +0700

    tesgt

--- Hidrologi/Analisis_Infiltrasi_CN.py
+++ Hidrologi/Analisis_Infiltrasi_CN.py
@@ -91,12 +91,12 @@ def calculate_limpasan(P, ARF, CN, Im):
     # Plotting bars on the first plot
     bar1 = fig.vbar(x=absis, top=Pkum, width=0.4, color="powderblue", legend_label="Hujan Rencana (ARF) [mm]")
     bar2 = fig.vbar(x=absis, top=reff_kum, width=0.4, color="orange", legend_label="Hujan efektif [mm]")
-    bar3 = fig.vbar(x=absis, top=infill_kum, width=0.4, color="greenyellow", legend_label="Infiltrasi [mm]")
+    bar3 = fig.vbar(x=absis, top=infiltrasi_kum, width=0.4, color="greenyellow", legend_label="Infiltrasi [mm]")
 
     # Adding text annotations
     for i in range(len(reff_kum)):
         fig.text(x=absis[i], y=reff_kum[i], text=[str(round(reff_kum[i], 1))], text_align='center', text_baseline='bottom')
-        fig.text(x=absis[i], y=infill_kum[i], text=[str(round(infill_kum[i], 1))], text_align='center', text_baseline='bottom')
+        fig.text(x=absis[i], y=infiltrasi_kum[i], text=[str(round(infiltrasi_kum[i], 1))], text_align='center', text_baseline='bottom')
 
     # Set axis labels and title
     fig.xaxis.axis_label = 'Absis'
@@ -109,12 +109,12 @@ def calculate_limpasan(P, ARF, CN, Im):
     fig2 = figure(width=600, height=400, title="Grafik Hujan Jam-Jaman dengan P = {} mm/hari".format(np.round(np.sum(P) / ARF,3)))
     bar4 = fig2.vbar(x=absis, top=P, width=0.4, color="powderblue", legend_label="Hujan Rencana (ARF) [mm]")
     bar5 = fig2.vbar(x=absis, top=reff, width=0.4, color="orange", legend_label="Hujan efektif [mm]")
-    bar6 = fig2.vbar(x=absis, top=infill, width=0.4, color="greenyellow", legend_label="Infiltrasi [mm]")
+    bar6 = fig2.vbar(x=absis, top=infiltrasi_jam, width=0.4, color="greenyellow", legend_label="Infiltrasi [mm]")
 
     # Adding text annotations
     for i in range(len(reff)):
         fig2.text(x=absis[i], y=reff[i], text=[str(round(reff[i], 1))], text_align='center', text_baseline='bottom')
-        fig2.text(x=absis[i], y=infill[i], text=[str(round(infill[i], 1))], text_align='center', text_baseline='bottom')
+        fig2.text(x=absis[i], y=infiltrasi_jam[i], text=[str(round(infiltrasi_jam[i], 1))], text_align='center', text_baseline='bottom')
 
     # Set axis labels and title
     fig2.xaxis.axis_label = 'Absis'
